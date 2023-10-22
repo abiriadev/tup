2d5bd0c8c2f48ddeb2dbe36cf0e188b692a374d0
Abiria <abiria.dev@gmail.com>
2023-10-23 02:47:48 +0900

    feat: define `Tup` and boilerplate parser logic

--- src/tup.rs
+++ src/tup.rs
@@ -1,3 +1,20 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{TokenStream, TokenTree};
+use syn::{parse::Parse, parse2};
 
-pub fn tup(input: TokenStream) -> TokenStream { input }
+struct Tup {
+	rep: usize,
+	tt: TokenTree,
+}
+
+impl Parse for Tup {
+	fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> { todo!() }
+}
+
+pub fn tup(input: TokenStream) -> TokenStream {
+	let res = match parse2::<Tup>(input) {
+		Ok(v) => v,
+		Err(e) => return e.into_compile_error(),
+	};
+
+	todo!()
+}
