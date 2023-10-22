3501b0a3bcd58367b86b6144ea96e67257bcfb47
Abiria <abiria.dev@gmail.com>
2023-10-23 02:56:33 +0900

    feat: implement parser

--- src/tup.rs
+++ src/tup.rs
@@ -1,5 +1,8 @@
 use proc_macro2::{TokenStream, TokenTree};
-use syn::{parse::Parse, parse2};
+use syn::{
+	parse::{Parse, ParseStream},
+	parse2, LitInt, Token,
+};
 
 struct Tup {
 	rep: usize,
@@ -7,7 +10,18 @@ struct Tup {
 }
 
 impl Parse for Tup {
-	fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> { todo!() }
+	fn parse(input: ParseStream) -> syn::Result<Self> {
+		let tt = input.parse::<TokenTree>()?;
+
+		input.parse::<Token![;]>()?;
+
+		let rep = input
+			.parse::<LitInt>()?
+			.base10_parse::<usize>()
+			.unwrap();
+
+		Ok(Self { rep, tt })
+	}
 }
 
 pub fn tup(input: TokenStream) -> TokenStream {
