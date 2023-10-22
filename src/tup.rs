fac8288d6a3a3abd9b9695d3afa1336befc5bdf0
Abiria <abiria.dev@gmail.com>
2023-10-23 03:41:44 +0900

    feat: use parenthesized tokentree

--- src/tup.rs
+++ src/tup.rs
@@ -1,4 +1,6 @@
-use proc_macro2::{Punct, Spacing, TokenStream, TokenTree};
+use std::iter::once;
+
+use proc_macro2::{Delimiter, Group, Punct, Spacing, TokenStream, TokenTree};
 use syn::{
 	parse::{Parse, ParseStream},
 	parse2, LitInt, Token,
@@ -30,12 +32,16 @@ pub fn tup(input: TokenStream) -> TokenStream {
 		Err(e) => return e.into_compile_error(),
 	};
 
-	(0..res.rep)
-		.into_iter()
-		.map(|_| {
-			let mut ts: TokenStream = res.tt.clone().into();
-			ts.extend([TokenTree::Punct(Punct::new(',', Spacing::Alone))]);
-			ts
-		})
-		.collect::<TokenStream>()
+	TokenTree::Group(Group::new(
+		Delimiter::Parenthesis,
+		(0..res.rep)
+			.into_iter()
+			.map(|_| {
+				let mut ts: TokenStream = res.tt.clone().into();
+				ts.extend([TokenTree::Punct(Punct::new(',', Spacing::Alone))]);
+				ts
+			})
+			.collect(),
+	))
+	.into()
 }
